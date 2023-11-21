bfdbf3aef6710475f1e6818d249554192f6b263a
SamPiter78 <109030148+SamPiter78@users.noreply.github.com>
2023-11-21 10:44:26 -0400

    Delete projecto de programacion directory

--- projecto de programacion/p/script.js
+++
@@ -1,6 +0,0 @@
-function showDetails(movieName) {
-    alert("Has seleccionado: " + movieName);
-}
-document.getElementById("backButton").addEventListener("click", function() {
-    window.location.href = "../pag2.html";
-});
